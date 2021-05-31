cf53fa965dc24dbe721118cfb31949441163416a
Nikhil <nikhilkumar.thera@gmail.com>
2021-05-31 20:26:21 +0530

    parameterized-job

---
+++ parameterized-job.groovy
@@ -0,0 +1,18 @@
+serversMap = [dev:"1.1.1.1"; test:"1.1.1.2"; uat:"1.1.1.3";]
+
+pipeline{
+    agentany
+    parameters {
+  choice choices: ['dev', 'uat', 'test'], description: 'choose an app to deploy', name: 'servers'
+         choices: ['finaltest', 'master', 'tomcat'], description: 'choose th ebranch to deploy', name: 'branchname'
+}
+    {stages{
+        stage ('maven and tomcat'){
+            sh "git checkout ${params.branchname}"
+            echo"we are building ${params.branchname}"
+            echo"we are deploying to ${params.servers} environment with ip ${serversmap[params.servers]}"
+                    }
+
+    }
+    }
+}
